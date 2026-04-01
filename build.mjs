4bd197b3191f813df036fb340206159bb1f77f46
Fabian Bouché <fabian.bouche@liferay.com>
2026-04-01 13:38:06 +0200

    initial commit

---
+++ build.mjs
@@ -0,0 +1,24 @@
+import {build} from 'esbuild';
+
+const EXTERNAL_PREFIXES = [
+	'@ckeditor/',
+	'@clayui/',
+	'@liferay/',
+	'react',
+	'react-dom',
+];
+
+const external = (id) => {
+	return EXTERNAL_PREFIXES.some((prefix) => id.startsWith(prefix));
+};
+
+await build({
+	bundle: true,
+	entryPoints: ['src/index.ts'],
+	format: 'esm',
+	outdir: 'build',
+	entryNames: '[name]-[hash]',
+	external: EXTERNAL_PREFIXES.map((p) => `${p}*`),
+});
+
+console.log('Build completed');
